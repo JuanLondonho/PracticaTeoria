a5b41b297acddf4c8de3b972d85b6c344e0f687c
carolina <caro.gc197@gmail.com>
2019-08-06 20:24:21 -0500

    Detecta si es deterministico o no

--- src/Modelo/Detectar.java
+++ src/Modelo/Detectar.java
@@ -5,18 +5,58 @@
  */
 package Modelo;
 
+
 /**
  *
  * @author juanclg
  */
 public class Detectar {
-    
-    public Detectar(){
-        
+
+    public Detectar() {
+
     }
-    
-    public boolean reconocer(String[][] matriz){
+
+    public boolean reconocer(String[][] matriz) {
+        String estado;
+        int cont = 0;
+        for (int i = 0; i < matriz[0].length-1; i++) {
+            for (int j = 1; j < matriz.length; j++) {
+                estado = matriz[j][i];
+                 if (i==0 && (estado.indexOf("*")) != -1) {
+                    cont = cont++;
+                    if (cont >= 2) 
+                       return true;
+                    
+                 }else if(i!=0 && estado.length()>1)
+                    return validarEstado(estado);            
+                
+            }
+        }
         
         return false;
     }
+    
+
+    public boolean validarEstado(String estado) {
+        if ((estado.indexOf("+")) != -1 && ((estado.startsWith("+") 
+            && estado.endsWith("+")) == false)) {
+            for (int n = 1; n < estado.length() - 1; n++) {
+                if (estado.charAt(n) == '+' && estado.charAt(n + 1) == '+') {
+                    return false;
+                    
+                }else 
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+
 }
+
+
+
+
+
+
+
